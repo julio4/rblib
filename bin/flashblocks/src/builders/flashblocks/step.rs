925efd53d1ccf7e0fd90710d8255852fb440fe35
karim <karim.dev@gmail.com>
2025-08-25 13:34:04 +0200

    More Flashblocks metrics

--- bin/flashblocks/src/builders/flashblocks/step.rs
+++ bin/flashblocks/src/builders/flashblocks/step.rs
@@ -7,7 +7,7 @@ use {
 			ExecutionPayloadFlashblockDeltaV1,
 		},
 	},
-	atomic_time::AtomicInstant,
+	atomic_time::AtomicOptionInstant,
 	core::sync::atomic::{AtomicU64, Ordering},
 	parking_lot::RwLock,
 	rblib::{
@@ -30,10 +30,6 @@ pub struct PublishFlashblock {
 	/// Keeps track of the current flashblock number within the block.
 	block_number: AtomicU64,
 
-	/// When the last block was published. Used to calculate flashblocks
-	/// duration.
-	last_block_at: AtomicInstant,
-
 	/// Set once at the begining of the payload job, captures immutable
 	/// information about the payload that is being built. This info is derived
 	/// from the payload attributes parameter on the FCU from the EL node.
@@ -41,6 +37,10 @@ pub struct PublishFlashblock {
 
 	/// Metrics for monitoring flashblock publishing.
 	metrics: Metrics,
+
+	/// Timestamps for various stages of the flashblock publishing process. This
+	/// information is used to produce some of the metrics.
+	times: Times,
 }
 
 impl PublishFlashblock {
@@ -50,7 +50,7 @@ impl PublishFlashblock {
 			block_number: AtomicU64::default(),
 			block_base: RwLock::new(None),
 			metrics: Metrics::default(),
-			last_block_at: AtomicInstant::now(),
+			times: Times::default(),
 		}
 	}
 }
@@ -71,8 +71,6 @@ impl Step<FlashBlocks> for PublishFlashblock {
 			// nothing to publish, empty flashblocks are not interesting, skip.
 			return ControlFlow::Ok(payload);
 		}
-
-		self.capture_metrics(&this_block_span);
 
 		// increment flashblock number
 		let index = self.block_number.fetch_add(1, Ordering::SeqCst);
@@ -99,6 +97,8 @@ impl Step<FlashBlocks> for PublishFlashblock {
 		}) {
 			tracing::error!("Failed to publish flashblock to websocket: {e}");
 		}
+		self.times.on_published_block(&self.metrics);
+		self.capture_payload_metrics(&this_block_span);
 
 		// Place a barrier after each published flashblock to freeze the contents
 		// of the payload up to this point, since this becomes a publicly committed
@@ -110,6 +110,8 @@ impl Step<FlashBlocks> for PublishFlashblock {
 		self: Arc<Self>,
 		ctx: StepContext<FlashBlocks>,
 	) -> Result<(), PayloadBuilderError> {
+		self.times.on_job_started(&self.metrics);
+
 		// this remains constant for the entire payload job.
 		self.block_base.write().replace(ExecutionPayloadBaseV1 {
 			parent_beacon_block_root: ctx
@@ -139,10 +141,11 @@ impl Step<FlashBlocks> for PublishFlashblock {
 		_: StepContext<FlashBlocks>,
 		_: Arc<Result<types::BuiltPayload<FlashBlocks>, PayloadBuilderError>>,
 	) -> Result<(), PayloadBuilderError> {
+		self.times.on_job_ended(&self.metrics);
+
 		// reset flashblocks block counter
 		let count = self.block_number.swap(0, Ordering::SeqCst);
 		self.metrics.blocks_per_payload_job.record(count as f64);
-
 		*self.block_base.write() = None;
 
 		Ok(())
@@ -153,7 +156,6 @@ impl Step<FlashBlocks> for PublishFlashblock {
 		ctx: InitContext<FlashBlocks>,
 	) -> Result<(), PayloadBuilderError> {
 		self.metrics = Metrics::with_scope(ctx.metrics_scope());
-		self.last_block_at.store(Instant::now(), Ordering::SeqCst);
 		Ok(())
 	}
 }
@@ -178,25 +180,22 @@ impl PublishFlashblock {
 		}
 	}
 
-	/// Called for each flashblock to capture metrics.
-	fn capture_metrics(&self, span: &Span<FlashBlocks>) {
+	/// Called for each flashblock to capture metrics about the produced
+	/// flashblock contents.
+	fn capture_payload_metrics(&self, span: &Span<FlashBlocks>) {
 		self.metrics.blocks_total.increment(1);
+		self.metrics.gas_per_block.record(span.gas_used() as f64);
 		self
 			.metrics
-			.gas_per_block_histogram
-			.record(span.gas_used() as f64);
+			.blob_gas_per_block
+			.record(span.blob_gas_used() as f64);
 		self
 			.metrics
-			.txs_per_block_histogram
+			.txs_per_block
 			.record(span.transactions().count() as f64);
 
-		let last_instant =
-			self.last_block_at.swap(Instant::now(), Ordering::SeqCst);
-		let elapsed = last_instant.elapsed();
-
-		if self.block_number.load(Ordering::SeqCst) > 0 {
-			self.metrics.intra_block_interval.record(elapsed);
-		}
+		let bundles_count = span.iter().filter(|c| c.is_bundle()).count();
+		self.metrics.bundles_per_block.record(bundles_count as f64);
 	}
 }
 
@@ -206,14 +205,107 @@ struct Metrics {
 	pub blocks_total: Counter,
 
 	/// Histogram of gas usage per flashblock.
-	pub gas_per_block_histogram: Histogram,
+	pub gas_per_block: Histogram,
+
+	/// Histogram of blob gas usage per flashblock.
+	pub blob_gas_per_block: Histogram,
 
 	/// Histogram of transactions per flashblock.
-	pub txs_per_block_histogram: Histogram,
+	pub txs_per_block: Histogram,
+
+	/// Histogram of the number of bundles per flashblock.
+	pub bundles_per_block: Histogram,
 
 	/// Histogram of flashblocks per job.
 	pub blocks_per_payload_job: Histogram,
 
-	/// The time interval between published flashblocks within one block.
+	/// The time interval flashblocks within one block.
 	pub intra_block_interval: Histogram,
+
+	/// The time interval between flashblocks from two consecutive payload jobs.
+	/// This measures time between last flashblock from block N and first
+	/// flashblock from block N+1.
+	pub inter_block_interval: Histogram,
+
+	/// The time interval between the end of one payload job and the start of the
+	/// next.
+	pub inter_jobs_interval: Histogram,
+
+	/// The time it takes between the beginning of a payload job
+	/// until the first flashblock is published.
+	pub time_to_first_block: Histogram,
+
+	/// The time beween the last published flashblock and the end of the payload
+	/// job.
+	pub idle_tail_time: Histogram,
+}
+
+#[derive(Default)]
+struct Times {
+	pub job_started: AtomicOptionInstant,
+	pub job_ended: AtomicOptionInstant,
+	pub first_block_at: AtomicOptionInstant,
+	pub previous_block_at: AtomicOptionInstant,
+	pub last_block_at: AtomicOptionInstant,
+}
+
+impl Times {
+	pub fn on_job_started(&self, metrics: &Metrics) {
+		let now = Instant::now();
+		self.job_started.store(Some(now), Ordering::Relaxed);
+
+		if let Some(ended_at) = self.job_ended.swap(None, Ordering::Relaxed) {
+			let duration = ended_at.duration_since(now);
+			metrics.inter_jobs_interval.record(duration);
+		}
+	}
+
+	pub fn on_job_ended(&self, metrics: &Metrics) {
+		let now = Instant::now();
+
+		if let Some(last_block_at) = self.last_block_at.load(Ordering::Relaxed) {
+			let idle_tail_time = now.duration_since(last_block_at);
+			metrics.idle_tail_time.record(idle_tail_time);
+		}
+
+		self.job_ended.store(Some(now), Ordering::Relaxed);
+		self.job_started.store(None, Ordering::Relaxed);
+		self.first_block_at.store(None, Ordering::Relaxed);
+		self.previous_block_at.store(None, Ordering::Relaxed);
+	}
+
+	pub fn on_published_block(&self, metrics: &Metrics) {
+		let now = Instant::now();
+		let last_block_at = self.last_block_at.load(Ordering::Relaxed);
+
+		if self
+			.first_block_at
+			.compare_exchange(None, Some(now), Ordering::Relaxed, Ordering::Relaxed)
+			.is_ok()
+		{
+			// this is the first block, capture inter-block interval
+			if let Some(last_block_at) = last_block_at {
+				let duration = now.duration_since(last_block_at);
+				metrics.inter_block_interval.record(duration);
+			}
+
+			// capture time to first block
+			let job_started = self.job_started.load(Ordering::Relaxed);
+			if let Some(job_started) = job_started {
+				let duration = now.duration_since(job_started);
+				metrics.time_to_first_block.record(duration);
+			}
+		}
+
+		// store now as the last block time
+		let prev_at = self.last_block_at.swap(Some(now), Ordering::Relaxed);
+		self.previous_block_at.store(prev_at, Ordering::Relaxed);
+
+		// capture the duration between consecutive flashblocks from the same
+		// payload job.
+		if let (Some(last_block_at), Some(prev_at)) = (last_block_at, prev_at) {
+			let duration = last_block_at.duration_since(prev_at);
+			metrics.intra_block_interval.record(duration);
+		}
+	}
 }
